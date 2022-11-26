38ba1d057452070a9f5d5971270b79ca76dfb194
Julian <julian.schmidt@axiomait.com>
2022-11-25 21:27:11 -0300

    algunos docs

--- Julian-Schmidt-Parcial2Labo1G/src/callbacks.h
+++ Julian-Schmidt-Parcial2Labo1G/src/callbacks.h
@@ -8,12 +8,45 @@
 #ifndef CALLBACKS_H_
 #define CALLBACKS_H_
 
+/// \brief Callback que filtra peliculas por genero drama
+///
+/// \param this Puntero a pelicula
+/// \return 1 (EXITO) o 0 (ERROR / no coincidencia)
 int callbackFilterDrama(void *this);
+
+/// \brief Callback que filtra peliculas por genero terror
+///
+/// \param this Puntero a pelicula
+/// \return 1 (EXITO) o 0 (ERROR / no coincidencia)
 int callbackFilterTerror(void *this);
+
+/// \brief Callback que filtra peliculas por genero accion
+///
+/// \param this Puntero a pelicula
+/// \return 1 (EXITO) o 0 (ERROR / no coincidencia)
 int callbackFilterAccion(void *this);
+
+/// \brief Callback que filtra peliculas por genero comedia
+///
+/// \param this Puntero a pelicula
+/// \return 1 (EXITO) o 0 (ERROR / no coincidencia)
 int callbackFilterComedia(void *this);
+
+/// \brief Callback que asigna valor aleatorio de rating
+///
+/// \param this puntero a elemento
 void callbackMap(void *this);
+
+/// \brief  Callback que asigna valor aleatorio de genero en base a un numero
+///
+/// \param this puntero a elemento
 void callbackMapGeneros(void *this);
+
+/// \brief Funcion que devuelve un valor dependiendo de el orden de las peliculas, con criterio por genero y luego por rating
+///
+/// \param pPrimerPelicula puntero a pelicula
+/// \param pSegundoPelicula puntero a pelicula
+/// \return Retorna 1 en caso de estar ordenados, -1 en caso de desordenados, y 0 en caso de igualdad o error
 int callbackOrdenarPorGenero(void *pPrimerPelicula, void *pSegundoPelicula);
 
 #endif /* CALLBACKS_H_ */
--- Julian-Schmidt-Parcial2Labo1G/src/peliculas.h
+++ Julian-Schmidt-Parcial2Labo1G/src/peliculas.h
@@ -18,10 +18,24 @@ typedef struct {
 	int rating;
 } eMovie;
 
+/// \brief Constructor por defecto de pelicula
+///
+/// \return retorna puntero a pelicula
 eMovie* pel_new();
+
+/// \brief Constructor por parametros de pelicula
+///
+/// \param idStr id en forma de char
+/// \param tituloStr titulo en forma de char
+/// \param generoStr genero en forma de char
+/// \param ratingStr rating en forma de char
+/// \return
 eMovie* pel_newParametros(char *idStr, char *tituloStr, char *generoStr,
 		char *ratingStr);
 
+/// \brief Destructor de pelicula
+///
+/// \param this Puntero a pelicula
 void pel_delete(eMovie *this);
 
 int pel_getId(eMovie *this, int *id);
